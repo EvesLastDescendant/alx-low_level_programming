1bda022b9204fb18bc9f6c7fb5005eb79b16a8a9
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2022-09-09 08:13:20 -0700

    c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 int main(void)
 {
-	char _str[] = "Programming is like building a multilingual puzzle\n";
-
+	char _str[];/*variable declaration*/
+	_str[] = "Programming is like building a multilingual puzzle \n";
 	puts(_str);
-
 	return (0);
 }
