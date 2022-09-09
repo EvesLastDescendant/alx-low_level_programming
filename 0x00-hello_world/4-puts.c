ccf49065cfb28fc30ee54b84edccc8964941e377
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2022-09-09 09:27:19 -0700

    c puts function script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char _str[] = "Programming is like building a multilingual puzzle\n";
+	char _str[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(_str);
 
