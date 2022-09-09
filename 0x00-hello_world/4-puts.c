dfee04e347d26871c3ef683388693a51389d395a
EvesLastDescendant <clydewitoutabonnie@gmail.com>
2022-09-09 09:25:35 -0700

    c puts function script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main -prints out a string holds a void argument
+ * _str - of data type char; is assigned the string to be dsiplayed
+ * puts() function - used instead of printf
+ * return - the value of variable _str
+ */
 int main(void)
 {
-	/* declaration and initialisation*/
+	char _str[] = "Programming is like building a multilingual puzzle\n";
 
-	_str[] = "Programming is like building a multilingual puzzle \n";
-	
 	puts(_str);
-	
+
 	return (0);
 }
